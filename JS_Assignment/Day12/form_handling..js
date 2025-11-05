20448a5419cb549e90f8cbe0aeeb192adb2a8ea9
PriyaDarshini-E <193783483+PriyaDarshini-E@users.noreply.github.com>
2025-11-04 20:34:52 -0500

    setInterval

--- JS_Assignment/Day12/form_handling..js
+++ JS_Assignment/Day12/form_handling..js
@@ -3,25 +3,25 @@ let img = document.getElementById("images")
 let submit = document.getElementById("submit_btn")
 let actress_Keerthy = document.getElementById("Keerthy")
 
-form.addEventListener("click", (e) => {
+form.addEventListener("click", (e) => { 
     e.preventDefault();
 
     // const actress = document.getElementById("actress").value
 
     const query = document.getElementById('actress').value;
-        // if (!query) {
-        //     alert('Please select an option');
-        //     return
-        // }
-    // } else if (query === actress_Keerthy) {
-    //     img.src = "../Day12/Image/keerthy1.jpg"
-    // }
+        if (!query) {
+            alert('Please select an option');
+            return
+        }
+     else if (query === "Samantha") {
+        img.src = "../Day12/Image/keerthy1.jpg"
+    }
 })
 
-    for(let i=1;i<=10;i++)
-    {
-        const image =document.createElement("img")
-        image.src = `images/${keerthy1}/${i}`;
-    // img.alt = `${actress} image ${i}`;
-    imageContainer.appendChild(img);
-    }
+    // for(let i=1;i<=10;i++)
+    // {
+    //     const image =document.createElement("img")
+    //     image.src = `images/${keerthy1}/${i}`;
+    // // img.alt = `${actress} image ${i}`;
+    // imageContainer.appendChild(img);
+    // }
