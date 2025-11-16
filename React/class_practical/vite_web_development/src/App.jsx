be707da819078a45e8da7f224c66e64715025724
PriyaDarshini-E <193783483+PriyaDarshini-E@users.noreply.github.com>
2025-11-16 18:10:21 -0500

    react task

--- React/class_practical/vite_web_development/src/App.jsx
+++ React/class_practical/vite_web_development/src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Chennai from "./components/Chennai/Chennai";
+import Karnataka from "./components/Karnataka/Karnataka";
+import Kerla from "./components/Kerla/Kerla";
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const App = () =>{
+
+return(
+  <>
+  <div><center>
+    <h1><u>This is a nested components Task</u></h1>
+  </center>  
+  </div>
+  <Karnataka/>
+  <Chennai/>
+  <Kerla/>
+  </>
+)
+
 }
 
-export default App
+export default App;
+
+
